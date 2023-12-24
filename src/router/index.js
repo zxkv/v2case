1c947f0544ad4152d69b1ba1ddd86942784caaa7
zxkv <zsk1115>
2023-12-25 00:51:45 +0800

    [up] env set

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import NProgress from "nprogress";
 import { routesBase, routesCommon } from "./router";
+
 NProgress.configure({ showSpinner: false });
 
 Vue.use(VueRouter);
 
-const hasProd = import.meta.env.VITE_APP_ENV === "production";
-
 const router = new VueRouter({
 	mode: "history",
-	base: hasProd ? "/v2case/" : "./",
+	base: "./",
 	routes: [...routesBase, ...routesCommon]
 });
 
